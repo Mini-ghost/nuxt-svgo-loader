7ee04293d445bfc853a1eae2525d59f3135b228e
Alex Liu <dsa1314@gmail.com>
2023-12-17 00:59:07 +0800

    feat: add folder-based data grouping

--- client/app.vue
+++ client/app.vue
@@ -23,6 +23,17 @@ const filtered = computed(() => {
   return result || []
 })
 
+const byFolders = computed(() => {
+  const result: Record<string, SvgFilesInfo[]> = {}
+  for (const asset of filtered.value) {
+    const folder = `/${asset.path.split('/').slice(0, -1).join('/')}`
+    if (!result[folder])
+      result[folder] = []
+    result[folder].push(asset)
+  }
+  return Object.entries(result).sort(([a], [b]) => a.localeCompare(b))
+})
+
 useEventListener('keydown', (event) => {
   switch (event.key) {
     case 'Escape':
@@ -36,14 +47,22 @@ useEventListener('keydown', (event) => {
   <div class="h-screen overflow-auto">
     <NNavbar ref="navbar" v-model:search="search" class="border-gray/20 p-2" />
 
-    <div class="grid grid-cols-minmax-8rem gap-1 p-2">
-      <template v-for="file in filtered" :key="file.path">
-        <button type="button" class="flex flex-col items-center gap-1 p-2 rounded hover:bg-gray/5" :class="{ '!bg-green/10': selected === file }" @click="selected = file">
-          <SvgPreview class="h-30 w-30" :src="`/__nuxt-svgo-loader/svg/${file.path}`" :alt="file.name" />
-          <span class="text-xs">{{ file.name }}</span>
-        </button>
-      </template>
-    </div>
+    <template v-for="[folder, items] of byFolders" :key="folder">
+      <NSectionBlock
+        :text="folder"
+        :description="`${items.length} items`"
+        :padding="false"
+        container-class="grid grid-cols-minmax-8rem gap-1 px-2"
+      >
+        <template v-for="file in items" :key="file.path">
+          <button type="button" class="flex flex-col items-center gap-1 p-2 rounded hover:bg-gray/5" :class="{ '!bg-green/10': selected === file }" @click="selected = file">
+            <SvgPreview class="h-30 w-30" :src="`/__nuxt-svgo-loader/svg/${file.path}`" :alt="file.name" />
+            <span class="text-xs">{{ file.name }}</span>
+          </button>
+        </template>
+      </NSectionBlock>
+      <div class="h-px w-full bg-gray/15" />
+    </template>
 
     <NDrawer :model-value="!!selected" :top="navbar" class="w-120 border-gray/20" auto-close @close="selected = undefined">
       <SvgDetails v-if="selected" :selected="selected" />
@@ -62,5 +81,10 @@ img {
   -moz-user-drag: none;
   -o-user-drag: none;
   user-drag: none;
+}
+/* override devtools-ui-ki */
+
+.hover\:bg-active {
+  @apply hover:bg-gray/5;
 }
 </style>

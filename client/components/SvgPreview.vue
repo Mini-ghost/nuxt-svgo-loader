79787c5857e4edbc01b4bd4db4cc8714bf0698b3
Noah Chen <hi@noahchen.me>
2024-07-22 22:30:14 +0800

    fix: image overflow in preview box (#5)

--- client/components/SvgPreview.vue
+++ client/components/SvgPreview.vue
@@ -7,6 +7,6 @@ defineProps<{
 
 <template>
   <div class="flex items-center justify-center rounded border border-gray/5 p-1 object-cover overflow-hidden bg-[#9CA3AF]/5">
-    <img :src="src" alt="alt">
+    <img class="max-h-full" :src="src" alt="alt">
   </div>
 </template>

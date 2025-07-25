12fd1144b4fc441ac6c7309ef936f6247e87b0a5
Alex Liu <dsa1314@gmail.com>
2025-07-25 19:26:24 +0800

    chore: update playground code

--- playground/app/pages/index.vue
+++ playground/app/pages/index.vue
@@ -8,8 +8,17 @@ import VueSvg from '~/assets/svg/vue.svg?component'
     <NuxtSvg width="92" height="92" fill="#00DC82" />
     <VueSvg width="92" height="92" fill="#41B883" />
 
+    <!-- @vue-expect-error -->
     <SvgoIcon /> <!-- should remove -->
-    <SvgoIcon name="nuxt" width="92" height="92" fill="lightgray" />
-    <SvgoIcon name="vue" width="92" height="92" fill="lightgray" />
+
+    <!-- Basic Usage（PascalCase） -->
+    <SvgoIcon name="nuxt" width="92" height="92" fill="#00DC82" />
+    <SvgoIcon name="vue" width="92" height="92" fill="#41B883" />
+
+    <!-- kebab-case -->
+    <svgo-icon name="nuxt" width="92" height="92" fill="#00DC82" />
+
+    <!-- skipsvgo -->
+    <SvgoIcon name="vue" strategy="skipsvgo" width="92" height="92" fill="#41B883" />
   </div>
 </template>

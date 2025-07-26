d86cbc916bf4c9849f8115eabf074d2103446be8
Alex Liu <dsa1314@gmail.com>
2025-07-26 23:56:42 +0800

    fix(devtools): correct relative import path in `<SvgDetails>` component

--- client/app/components/SvgDetails.vue
+++ client/app/components/SvgDetails.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { SvgFilesInfo } from '../../src/types'
+import type { SvgFilesInfo } from '../../../src/types'
 import { genImport, genSafeVariableName } from 'knitwork'
 import { join } from 'pathe'
 import { camelCase, pascalCase } from 'scule'
